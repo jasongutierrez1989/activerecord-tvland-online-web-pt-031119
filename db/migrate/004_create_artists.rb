a641315e0cd6ee44b742546baca051ef843dc5b7
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-10 00:52:52 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_artists.rb
+++ db/migrate/004_create_artists.rb
@@ -1,8 +1,8 @@
 class CreateArtists < ActiveRecord::Migration
   def change
     create_table :artists do |t|
-      t.string first_name
-      t.string_ last_name
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
